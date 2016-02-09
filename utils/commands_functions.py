12f5a461f48b759ff0b623d3003502996ffbb1a5
Eder Xavier Rojas <edxavier05@gmail.com>
2016-02-09 17:52:24 -0600

    Refactor

--- utils/commands_functions.py
+++ utils/commands_functions.py
@@ -29,8 +29,8 @@ def take_snapshot(bot=None, chat_id=None, remote=None, ssh_user=None, ssh_passwo
             err = stderr.channel.recv_stderr(1024)
             if err:
                 print(err)
-                bot.send_message(chat_id, "	\xF0\x9F\x98\x94 \n<b>Lo siento no se pudo ejecutar la orden</b>", parse_mode="Html")
-                bot.send_message(chat_id, "	\xF0\x9F\x98\x94 \n<b>Error: %s </b>" % err, parse_mode="Html")
+                if err != "kbuildsycoca running...":
+                    bot.send_message(chat_id, "	\xF0\x9F\x98\x94 \n<b>Algo inesperado paso, la descripcion es: %s </b>" % err, parse_mode="Html")
             else:
                 bot.send_message(chat_id, "\xF0\x9F\x98\x8E \n<b>He enviado la orden verifica por favor</b>", parse_mode="Html")
 
