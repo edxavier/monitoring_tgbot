732b87a64b1d438be800b8fc43743acf78e9edce
Eder Xavier Rojas <edxavier05@gmail.com>
2016-02-09 17:47:03 -0600

    Refactor

--- utils/commands_functions.py
+++ utils/commands_functions.py
@@ -9,7 +9,7 @@ def take_snapshot(bot=None, chat_id=None, remote=None, ssh_user=None, ssh_passwo
     if established:
         try:
             bot.send_message(chat_id, "He enviado la orden...", parse_mode="Html")
-            channel = ssh.invoke_shell()
+            """channel = ssh.invoke_shell()
             stdin = channel.makefile('wb')
             stdout = channel.makefile('rb')
 
@@ -23,15 +23,16 @@ def take_snapshot(bot=None, chat_id=None, remote=None, ssh_user=None, ssh_passwo
 
             stdout.close()
             stdin.close()
-            #ssh_stdin, stdout, stderr = ssh.exec_command("/root/tg_snapshot.sh")
+            """
+            ssh_stdin, stdout, stderr = ssh.exec_command("export DISPLAY=:0 \n /usr/bin/ksnapshot")
             #print stdout.channel.recv_exit_status()
-            """err = stderr.channel.recv_stderr(1024)
+            err = stderr.channel.recv_stderr(1024)
             if err:
                 print(err)
                 bot.send_message(chat_id, "	\xF0\x9F\x98\x94 \n<b>Lo siento no se pudo ejecutar la orden</b>", parse_mode="Html")
                 bot.send_message(chat_id, "	\xF0\x9F\x98\x94 \n<b>Error: %s </b>" % err, parse_mode="Html")
             else:
-                bot.send_message(chat_id, "\xF0\x9F\x98\x8E \n<b>He enviado la orden verifica por favor</b>", parse_mode="Html")"""
+                bot.send_message(chat_id, "\xF0\x9F\x98\x8E \n<b>He enviado la orden verifica por favor</b>", parse_mode="Html")
 
         except:
             bot.send_message(chat_id, "\xF0\x9F\x92\xA5 \xF0\x9F\x98\xB5 <b>Ocurrio un inconveniente "
